48deb00c936cbc92566cc7a623e30ccebc456f85
Said ALX <said@alx.com>
2023-02-16 14:19:09 -0800

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * Write a C program that prints exactly 
+ * Description: Write a C program that prints exactly
  * "Programming is like building a multilingual puzzle
- * followed by a new line.
+ * followed by a new line
+ *
+ * Return: always returns 0
  */
 int main(void)
 {
