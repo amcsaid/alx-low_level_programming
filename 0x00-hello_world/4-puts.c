ec823d96e6dcf87a859c809f1828c205330dceb2
Said ALX <said@alx.com>
2023-02-16 14:16:53 -0800

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Write a C program that prints exactly 
+ * "Programming is like building a multilingual puzzle
+ * followed by a new line.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
 
 
