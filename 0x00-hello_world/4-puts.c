6b83dbf2215b24051543a3af6aa55f19f3890497
Said ALX <said@alx.com>
2023-02-16 14:20:56 -0800

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - Program start
  * Description: Write a C program that prints exactly
  * "Programming is like building a multilingual puzzle
  * followed by a new line
- *
  * Return: always returns 0
  */
 int main(void)
