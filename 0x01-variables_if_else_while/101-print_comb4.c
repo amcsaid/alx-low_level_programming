defc90cc1800d180ae6679b563e71adbe0a083d8
Said ALX <said@alx.com>
2023-02-17 15:18:33 -0800

    task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start of program
+ * Description: Randow number is positive or negative
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int n, m;
+
+	n = '0';
+	m = '0';
+	k = '0';
+
+	while (n <= '9')
+	{
+		m = n + 1;
+		while (m <= '9')
+		{
+			k = m + 1;
+			while(k <= '9')
+			{
+				putchar(n);
+				putchar(m);
+				putchar(k);
+				if ((n != '7') || (m != '8') || (k != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			k = '0';
+			m++;
+		}
+		m = '0';
+		n++;
+
+	}
+	putchar(10);
+	return (0);
+}
