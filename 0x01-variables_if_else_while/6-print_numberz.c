6b434adf55073a087e8d5bead6f0f1e6d82e3bc4
Said ALX <said@alx.com>
2023-02-17 14:29:53 -0800

    task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start of program
+ * Description: Randow number is positive or negative
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
