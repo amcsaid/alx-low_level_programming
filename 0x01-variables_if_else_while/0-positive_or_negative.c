064928c7577d8bcfeac930ce22a020bf2c00c8a5
Said ALX <said@alx.com>
2023-02-17 12:58:18 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		puts("is positive");
+		puts("%d is positive", n);
 	else if (n == 0)
-		puts("is zero");
+		puts("%d is zero", n);
 	else
-		puts("is negative");
+		puts("%d is negative", n);
 	return (0);
 }
 
