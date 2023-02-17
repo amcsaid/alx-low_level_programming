1bf6262bf0a75db5adf694397b1129d49f95429c
Said ALX <said@alx.com>
2023-02-17 14:15:56 -0800

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start of program
+ * Description: ALX Tasks
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		putchar(n);
+	}
+	for (n = 65; n < 91; n++)
+        {
+                putchar(n);
+        }
+	putchar(10);
+	return (0);
+}
