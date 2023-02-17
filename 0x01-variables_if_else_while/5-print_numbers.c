6922cfe67a9f71967c7bd32266925578950a900c
Said ALX <said@alx.com>
2023-02-17 14:28:04 -0800

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start of program
+ * Description: ALX Tasks
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar(10);
+	return (0);
+}
