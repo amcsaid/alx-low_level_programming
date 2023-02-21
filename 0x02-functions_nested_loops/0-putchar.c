1a582c60b67d8cd47ac7f969c4a515cb3bf093c3
Said ALX <said@alx.com>
2023-02-21 14:08:26 -0800

    alx tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - start of program
+ * Description: prints something
+ * Return: always returns 0
+ */
+int main(void)
+{
+	printf("_putchar");
+}
