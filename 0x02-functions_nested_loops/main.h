981a6edcd84d7df4a4d688281984ba5c9a5f417c
Said ALX <said@alx.com>
2023-02-21 15:14:39 -0800

    alx tasks

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -47,3 +47,19 @@ int _isalpha(int c)
 	else
 		return (0);
 }
+int print_sign(int n)
+{
+	if ( c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+	return (-1);
+}
