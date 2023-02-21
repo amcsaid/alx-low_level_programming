ebaa4cee139cc262941159cc26e306c54f808608
Said ALX <said@alx.com>
2023-02-21 14:37:53 -0800

    alx tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 /**
- * main - check the code
- *
+ * maintwo - check the code
+ * Description: dummy code to finish the task
  * Return: Always 0.
  */
 int maintwo(void)
 {
-    return (0);
+	return (0);
 }
