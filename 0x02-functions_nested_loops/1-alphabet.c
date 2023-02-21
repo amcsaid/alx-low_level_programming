9d74e9efe113a213a7f6c9705bf90dbe08b8e782
Said ALX <said@alx.com>
2023-02-21 14:29:41 -0800

    alx tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,5 @@
-#include <stdlib.h>
-#include "main.h"
 /**
  * main - start of program
  * Description: prints something
  * Return: always returns 0
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
