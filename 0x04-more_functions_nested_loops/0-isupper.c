1dba68711b22b3c7838a7eefaaff142380888866
Said ALX <said@alx.com>
2023-02-23 14:20:14 -0800

    tast

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _isupper - check the code
  * @c: integer input
  * Description: check if a caracter or not
  * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
 	if ( (c > 64) && (c < 91) )
