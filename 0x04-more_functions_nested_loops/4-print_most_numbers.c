31fafe6ed1ca3a58d521d6177a9022cd7f53c4f4
Said ALX <said@alx.com>
2023-02-23 16:24:10 -0800

    task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ * Description: prints numbers skipping 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		if ((i != 2) && (i != 4))
+			_putchar('0' + i);
+	_putchar('\n');
+}
