4cdee4fb8f7b2083dc323422742be780059daf99
Said ALX <said@alx.com>
2023-02-23 16:12:32 -0800

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * Description: prints numbers
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+		putchar('0' + i);
+}
