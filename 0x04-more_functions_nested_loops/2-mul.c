81e596e5b99fae26994233aafda662b868c2b12e
Said ALX <said@alx.com>
2023-02-23 16:06:40 -0800

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ * @a: integer input
+ * @b: integer input
+ * Description: multiplies two integers
+ * Return: multiplies two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
