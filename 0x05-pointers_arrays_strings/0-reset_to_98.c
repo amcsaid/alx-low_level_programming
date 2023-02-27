564491fbf3c2c53a49c2fa849f5a1ace214e5525
Said ALX <said@alx.com>
2023-02-27 14:07:34 -0800

    tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code
+ * @*n: integer pointer input
+ * Description: resets the value of the variable pointed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
