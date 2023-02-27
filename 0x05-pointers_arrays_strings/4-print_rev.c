6c8d9525916a7800faddba223b71fe3735d4980a
Said ALX <said@alx.com>
2023-02-27 14:48:19 -0800

    tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: char pointer as an input
+ * Description: Pointers manipulation
+ */
+
+void print_rev(char *s)
+{
+	int lenght = _strlen(s);
+	int *last = s + lenght;
+
+
+	while (last != s)
+	{
+		_putchar(*last);
+		last--;
+	}
+	_putchar('\n');
+}
