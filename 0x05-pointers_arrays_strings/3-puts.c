a2519d2c7be3371cb76715f70d5a39f74dfb283c
Said ALX <said@alx.com>
2023-02-27 14:37:25 -0800

    tasks

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _puts - check the code
+ * @s: char pointer as an input
+ * Description: Pointers manipulation
+ */
+
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		_purchar(*s);
+		s++;
+	}
+}
