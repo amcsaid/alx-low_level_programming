f7df743e4242d09e051d63ae24c3691bbb6ccf38
Said ALX <said@alx.com>
2023-02-27 14:59:58 -0800

    tasks

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -16,15 +16,3 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
-int _strlen(char *s)
-{
-        int count = 0;
-
-        while (*s != '\0')
-        {
-                s++;
-                count++;
-        }
-
-        return (count);
-}
