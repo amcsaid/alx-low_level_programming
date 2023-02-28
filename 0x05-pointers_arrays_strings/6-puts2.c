3d7fdef879e8baefd21a9517a2ad9980817f6cb4
Said ALX <said@alx.com>
2023-02-27 17:37:23 -0800

    task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * puts2 - check the code
+ * @str: char pointer as an input
+ * Description: Pointers manipulation
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		if (i%2 == 0)
+			_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
