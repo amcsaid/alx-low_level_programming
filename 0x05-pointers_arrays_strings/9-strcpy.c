dbc28be672c7c960c5f3ce9c969448a6f3632b31
Said ALX <said@alx.com>
2023-02-28 05:43:15 -0800

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code
+ * @dest: array pointer
+ * @src: array pointer
+ * Description: Pointers manipulation
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = _strlen(src);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
+/**
+ * _strlen - check the code
+ * @s: char pointer as an input
+ * Description: Pointers manipulation
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	return (count);
+}
