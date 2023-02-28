09960b1fbb8660ef5c6f78811ae5238d0fd0bbc0
Said ALX <said@alx.com>
2023-02-27 18:13:11 -0800

    task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,12 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
-	printf("%d", a[i++]);
+
+	if ( n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", a[i]);
+		printf("%d", a[i++]);
+	}
 	printf("\n");
 }
