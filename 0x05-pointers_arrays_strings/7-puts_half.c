9969136c77fd9e3067d9152f8fed80eeefcf4b57
Said ALX <said@alx.com>
2023-02-27 17:46:37 -0800

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _puts - check the code
+ * @str: char pointer as an input
+ * Description: Pointers manipulation
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+/**
+ * _puts - check the code
+ * @str: char pointer as an input
+ * Description: Pointers manipulation
+ */
+
+void puts_half(char *str)
+{
+	int length_of_the_string = _strlen(str);
+	int n = (length_of_the_string - 1) / 2;
+
+	_puts(str + (length_of_the_string - n));
+
+}
+/**
+ * _strlen - check the code
+ * @s: char pointer as an input
+ * Description: Pointers manipulation
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	return (count);
+}
