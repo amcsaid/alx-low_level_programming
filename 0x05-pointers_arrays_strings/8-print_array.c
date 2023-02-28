f8fa274b136079ce5387644c293e62b11a46f239
Said ALX <said@alx.com>
2023-02-27 18:02:45 -0800

    task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 
 /**
- * _puts - check the code
- * @str: char pointer as an input
- * Description: Pointers manipulation
- */
-
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-}
-/**
  * puts_half - check the code
  * @str: char pointer as an input
  * Description: Pointers manipulation
@@ -25,28 +10,9 @@ void _puts(char *str)
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%f", a++);
+	
+	for (i = 0; i < n; i++) {
+		printf("%d ", a[i]);
 	}
-}
-/**
- * _strlen - check the code
- * @s: char pointer as an input
- * Description: Pointers manipulation
- * Return: lenght of string
- */
-
-int _strlen(char *s)
-{
-	int count = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		count++;
-	}
-
-	return (count);
+	printf("\n");
 }
