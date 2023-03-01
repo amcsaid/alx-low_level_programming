8c7ab98e1c868e748671afc52c2a769f854ce0d2
Said ALX <said@alx.com>
2023-03-01 14:36:09 -0800

    tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,9 @@
 /**
  * _strcat - check the code
  * @dest: char pointer as an input
- * @char: char pointer as an input
+ * @src: char pointer as an input
  * Description: Pointers manipulation
+ * Return: pointer to result string
  */
 
 char *_strcat(char *dest, char *src)
