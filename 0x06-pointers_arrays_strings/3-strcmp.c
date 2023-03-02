92f88dcb53df354ebd8d67cb11de2f25762ed7a0
Said ALX <said@alx.com>
2023-03-02 13:32:37 -0800

    tast

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,8 +17,9 @@ int _strcmp(char *s1, char *s2)
 		c1 = *s1++;
 		c2 = *s2++;
 		if (c1 == '\0')
-			return c1 - c2;
-	} while (c1 == c2)
+			return (c1 - c2);
+	} while (c1 == c2);
+	
 
-	return c1 - c2;
+	return (c1 - c2);
 }
