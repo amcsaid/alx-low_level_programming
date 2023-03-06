ebbf5fc7c488ee4fd9b83d0e54d16e941c70c278
Said ALX <said@alx.com>
2023-03-06 14:17:42 -0800

    task

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *  _strspn - check the code
+ * @s: pointer to string to search
+ * @accept: char to search for
+ * Description: Pointers manipulation
+ * Return: uppercase string point
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	char *c;
+	int t = 0, i = 0;
+
+	for (c = accept; *c; c++)
+	{
+		while (*s != '\0')
+		{
+			if (*s == c)
+				t++;
+			s++;
+			i++;
+		}
+		s = s - i;
+		i = 0;
+	}
+	return (t);
+}
