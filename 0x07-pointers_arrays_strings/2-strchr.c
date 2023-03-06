b13e2c07333306d7689daeb4842464ef00225698
Said ALX <said@alx.com>
2023-03-06 12:51:04 -0800

    task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - check the code
+ * @s: pointer to string to search
+ * @c: char to search for
+ * Description: Pointers manipulation
+ * Return: uppercase string point
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return(s);
+		s++;
+	}
+	return (NULL);
+}
