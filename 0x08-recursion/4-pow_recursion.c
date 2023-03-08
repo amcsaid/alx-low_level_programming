5dbf4ccdc7cbd53d8a0a3c99c9e009e8e2ea2553
Said ALX <said@alx.com>
2023-03-08 15:24:50 -0800

    task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _pow_recursion - check the code
+ * @x: int intput
+ * @y: int input
+ * Description: Pointers manipulation
+ * Return: factorial of a number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x, _pow_recursion(y - 1));
+	}
+	else if (y == 0)
+		return (1);
+}
