887e8aacda51d7a88a527071cf05b413fd27cd37
Said ALX <said@alx.com>
2023-03-08 15:08:02 -0800

    task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen_recursion - check the code
+ * @s: char pointer as an input
+ * Description: Pointers manipulation
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+	else
+		return (0);
+}
