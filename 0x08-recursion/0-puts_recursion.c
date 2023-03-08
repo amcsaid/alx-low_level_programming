d8dc4e3f6723ff1e38fd3bed4894f511a601ebe7
Said ALX <said@alx.com>
2023-03-08 14:19:08 -0800

    task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,10 @@
 void _puts_recursion(char *s)
 {
 	if (s[0] == '\0')
-                _putchar('\n');
-        else
-                _putchar(s[0]);
-	
+		_putchar('\n');
+	else
+		_putchar(s[0]);
+
 	if (s[0] != '\0')
 	{
 		s++;
