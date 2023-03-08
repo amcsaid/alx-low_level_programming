33d5556db3f087aa97985477ee029bfef409dcba
Said ALX <said@alx.com>
2023-03-08 15:48:08 -0800

    task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _sqrt_recursion - check the code
+ * @n: int intput
+ * Description: Pointers manipulation
+ * Return: result of op on the number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n > 2)
+		return (x * _pow_recursion(x, y - 1));
+	else if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	return (0);
+}
