1906c404bc3d55481f8b24a507b67c1a4b77cb35
Said ALX <said@alx.com>
2023-03-08 15:13:07 -0800

    task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * factorial - check the code
+ * @n: char pointer as an input
+ * Description: Pointers manipulation
+ * Return: factorial of a number
+ */
+int factorial(int n)
+{
+	if (n != 0)
+	{
+		return (n * factorial(n-1));
+	}
+	else
+		return (1);
+}
