ba13eab36f0cb8c68aaaf62910ca785ed373f21d
Said ALX <said@alx.com>
2023-03-09 14:02:02 -0800

    task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,14 +10,7 @@ void _print_rev_recursion(char *s)
 {
 	if (s[0] != '\0')
 	{
-		s++;
-		_print_rev_recursion(s);
-		if (s[-1] == '\n')
-		{
-			_putchar(s[0]);
-			_putchar('\n');
-		}
-		else
-			_putchar(s[0]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
