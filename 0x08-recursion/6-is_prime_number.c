c0acef9aa93f5aa986523bde2d96f7b5bd23719b
Said ALX <said@alx.com>
2023-03-09 14:11:45 -0800

    task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,10 +9,8 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 0)
-		return (0);
-	else if (n > 1)
-		return (is_prime_number(n - 1));
+	if (n > 1)
+		return (is_prime_number_helper(n, 2));
 	return (0);
 }
 
