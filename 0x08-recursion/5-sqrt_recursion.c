a4043a7a79ea3e7641e96d3420471c1ac123feb3
Said ALX <said@alx.com>
2023-03-09 13:58:37 -0800

    task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,29 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n > 2)
-		return (x * _pow_recursion(x, y - 1));
-	else if (y == 0)
+	if (n == 0)
+		return (0);
+	else if (n == 1)
 		return (1);
-	else if (y < 0)
+	else if (n < 0)
 		return (-1);
-	return (0);
+	else
+		return (whatroot(n, 1));
+}
+
+/**
+ * whatroot - check the code
+ * @n: int intput
+ * @i: increments
+ * Description: help funtion
+ * Return: result of op on the number
+ */
+int whatroot(int n, int i)
+{
+	if (n == i * i)
+		return (i);
+	else if (n > (i * i))
+		return (whatroot(n, i + 1));
+	else
+		return (-1);
 }
