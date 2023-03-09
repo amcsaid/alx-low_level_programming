b71828f4df73694564954274a9af77d037122028
Said ALX <said@alx.com>
2023-03-09 14:08:34 -0800

    task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,3 +15,23 @@ int is_prime_number(int n)
 		return (is_prime_number(n - 1));
 	return (0);
 }
+
+/**
+ * is_prime_number_help - check the code
+ * @n: int input
+ * @i: increments
+ * Description: Recursion manipulation
+ * Return: is prime or not
+ */
+int is_prime_number_help(int n, int i)
+{
+	if (n % i == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (is_prime_number_help(n, i + 1) + 0);
+}
+
