ec0f8c34d16ff78633c6b8ac756d6c6300739271
Said ALX <said@alx.com>
2023-03-10 15:13:53 -0800

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ * @argc: number of args
+ * @argv: array of strings
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	
+	if (argc == 3)
+	{
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	} else
+		printf("Error\n");
+	return (1);
+}
