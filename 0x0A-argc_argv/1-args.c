b9e381434d85603103e435dd382dc70e60e77a1a
Said ALX <said@alx.com>
2023-03-10 14:57:55 -0800

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ * @argc: number of args
+ * @argv: array of strings
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	exit(EXIT_SUCCESS);
+}
