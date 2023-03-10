c65dc851e599436e7a07b11b5b653ef15bcd1e95
Said ALX <said@alx.com>
2023-03-10 14:54:14 -0800

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
