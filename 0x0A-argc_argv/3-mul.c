7259d809dccb754ab7d82506cbcfd37932ff16c1
Said ALX <said@alx.com>
2023-03-10 15:15:32 -0800

    task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,10 @@
  */
 int main(int argc, char **argv)
 {
-	
 	if (argc == 3)
 	{
 		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
 	} else
 		printf("Error\n");
-	return (1);
+	return (0);
 }
