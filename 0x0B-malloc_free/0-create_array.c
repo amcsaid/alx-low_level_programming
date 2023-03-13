5ea266b7289b28cdb4e9508fb824768bd60af8aa
Said ALX <said@alx.com>
2023-03-13 16:24:05 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * create_array - check the code
@@ -11,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ar;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
