88f548cb001668269038cfaeb31c9bc434d81353
Said ALX <said@alx.com>
2023-03-13 16:32:48 -0700

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - check the code
+ * @str: pinter to string
+ * Description: Malloc usage
+ * Return: char pointer
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int i, size;
+
+	size = strlen(str);
+
+	if (size == 0)
+		return (NULL);
+
+	ar = malloc(size * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < size ; i++)
+		ar[i] = str[i];
+
+	return (ar);
+}
