9122ce2fc60338aa0ce593366420026985c3ae40
Said ALX <said@alx.com>
2023-03-13 16:54:18 -0700

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,17 +13,16 @@ char *_strdup(char *str)
 	char *ar;
 	int size;
 
-	size = strlen(str) + 1;
-
 	if (size == 0 || str == NULL)
 		return (NULL);
 
+	size = strlen(str) + 1;
 	ar = malloc(size * sizeof(char));
 
 	if (ar == NULL)
 		return (NULL);
 
-	memcpy (ar, str, size);
+	memcpy(ar, str, size);
 
 	return (ar);
 }
