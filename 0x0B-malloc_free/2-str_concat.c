578ba3432b4b017b9f0f5777e1efe41defe0dca6
Said ALX <said@alx.com>
2023-03-15 18:40:58 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,23 +13,23 @@
 char *str_concat(char *s1, char *s2) {
 	int s2_len, s1_len;
 	char *result;
-    if (s1 == NULL) {
-        s1 = "";
-    }
-    if (s2 == NULL) {
-        s2 = "";
-    }
+	if (s1 == NULL) {
+		s1 = "";
+	}
+	if (s2 == NULL) {
+		s2 = "";
+	}
 
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
-    result = malloc((s1_len + s2_len + 1) * sizeof(char));
-    if (result == NULL) {
-        return NULL;
-    }
+	result = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (result == NULL) {
+		return NULL;
+	}
 
-    memcpy(result, s1, s1_len);
-    memcpy(result + s1_len, s2, s2_len + 1);
+	memcpy(result, s1, s1_len);
+	memcpy(result + s1_len, s2, s2_len + 1);
 
-    return result;
+	return result;
 }
