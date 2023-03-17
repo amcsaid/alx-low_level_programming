d911697b82e26ad04f9075ccbba1019cdafbe5a5
Said ALX <said@alx.com>
2023-03-16 18:14:40 -0700

    task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - Allocates memory for an array of ints
+ *
+ * @ptr: starting int
+ * @old_size: ending int
+ * @new_size
+ *
+ * Return: A pointer to the allocated memory, or NULL if nmemb or size is 0
+ *         or if malloc fails.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	return (ptr);
+}
