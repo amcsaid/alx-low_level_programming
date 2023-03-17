75b0e3523a5a5879eabe2c868f76a4cf2a6d41af
Said ALX <said@alx.com>
2023-03-16 17:40:25 -0700

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,37 +14,37 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concat_str;
-    unsigned int s1_len = 0, s2_len = 0;
+	char *concat_str;
+	unsigned int s1_len = 0, s2_len = 0;
 
-    /* If s1 is NULL, treat it as an empty string */
-    if (s1 == NULL)
-        s1 = "";
+	/* If s1 is NULL, treat it as an empty string */
+	if (s1 == NULL)
+		s1 = "";
 
-    /* If s2 is NULL, treat it as an empty string */
-    if (s2 == NULL)
-        s2 = "";
+	/* If s2 is NULL, treat it as an empty string */
+	if (s2 == NULL)
+		s2 = "";
 
-    /* Get the length of s1 and s2 */
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
+	/* Get the length of s1 and s2 */
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
-    /* If n is greater than or equal to s2_len, use the entire string s2 */
-    if (n >= s2_len)
-        n = s2_len;
+	/* If n is greater than or equal to s2_len, use the entire string s2 */
+	if (n >= s2_len)
+		n = s2_len;
 
-    /* Allocate memory for the concatenated string */
-    concat_str = malloc(s1_len + n + 1);
-    if (concat_str == NULL)
-        return (NULL);
+	/* Allocate memory for the concatenated string */
+	concat_str = malloc(s1_len + n + 1);
+	if (concat_str == NULL)
+		return (NULL);
 
-    /* Copy s1 and the first n bytes of s2 into concat_str */
-    memcpy(concat_str, s1, s1_len);
-    memcpy(concat_str + s1_len, s2, n);
+	/* Copy s1 and the first n bytes of s2 into concat_str */
+	memcpy(concat_str, s1, s1_len);
+	memcpy(concat_str + s1_len, s2, n);
 
-    /* Add a null terminator to the end of the concatenated string */
-    concat_str[s1_len + n] = '\0';
+	/* Add a null terminator to the end of the concatenated string */
+	concat_str[s1_len + n] = '\0';
 
-    /* Return a pointer to the concatenated string */
-    return (concat_str);
+	/* Return a pointer to the concatenated string */
+	return (concat_str);
 }
