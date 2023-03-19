f7ada0efe1e8a3b6012a0cabc05888dadc6063b4
Said ALX <said@alx.com>
2023-03-19 09:38:59 -0700

    task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Program start
+ * Description: Print datatype sizes
+ * Return: always returns 0
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	while (*s)
+	{
+		write (2, s, 1);
+		s++;
+	}
+	return (0);
+}
+
+
