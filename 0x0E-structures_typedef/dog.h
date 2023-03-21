fda0dd6be0bcf41cfb6534a8bb97d68ec1e50194
Said ALX <said@alx.com>
2023-03-20 18:53:06 -0700

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef GRANDPARENT_H
+#define GRANDPARENT_H
+/**
+ * struct dog - structure representing a dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* GRANDPARENT_H */
