cac257acde11270a8bdbb7be1d9e85fabed8e02b
Said ALX <said@alx.com>
2023-03-22 17:34:10 -0700

    task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+/**
+ * print_name - calls a function to print a name
+ * @name: name to be printed
+ * @f: function pointer to the printing function
+ *
+ * This function takes a string `name` and a function pointer `f` as arguments,
+ * and calls the function `f` with `name` as its argument.
+ *
+ * Return: none
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
